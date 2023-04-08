7e5e562924d6e2a5ae884dbb602a031fbc15f03d
Cailyn Yong <cailynyongyong@gmail.com>
2023-04-08 16:51:42 +0400

    feature: feed upload posts

--- components/Publication.js
+++ components/Publication.js
@@ -1,7 +1,14 @@
 // components/Publication.js
-
-import Link from "next/link";
+import { Fragment, useState } from "react";
 export default function Publication() {
+  const [content, setContent] = useState("");
+  const [contentList, setContentList] = useState([]);
+
+  async function post() {
+    console.log("entering post function");
+    setContentList([...contentList, content]);
+  }
+
   return (
     <div>
       <div class="col-span-full">
@@ -13,10 +20,9 @@ export default function Publication() {
         </label>
         <div class="mt-2">
           <textarea
-            id="about"
-            name="about"
             rows="3"
             class="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+            onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </div>
       </div>
@@ -24,11 +30,28 @@ export default function Publication() {
         <button
           type="submit"
           class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          //onClick={}
+          onClick={post}
         >
           Upload
         </button>
       </div>
+      {contentList?.length > 0
+        ? contentList.map((e, v) => {
+            return (
+              <div className="p-8">
+                <div className="max-w-md mx-auto overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
+                  <div className="md:flex">
+                    <div className="p-8">
+                      <p className="mt-2 text-xs whitespace-pre-line text-slate-500">
+                        {e}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        : "false"}
     </div>
   );
 }
